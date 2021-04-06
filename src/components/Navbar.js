5e75f4d4d8f354c11c233445884647ad5d853778
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-06 15:28:57 -0400

    added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return ( 
+    <nav className="navbar">
+      <Link to='/'><h1>Roots</h1></Link>
+      <div className="links">
+      <Link to='/families/new'>Add Family</Link>
+      &nbsp;
+      <Link to='/families'>All Families</Link>
+      </div>
+    </nav>
+   );
+}
+ 
+export default Navbar;
